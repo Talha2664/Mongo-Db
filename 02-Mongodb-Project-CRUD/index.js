8b57dc0cc9b6a11c4958f75845bfd7edab0c2597
Talha2664 <talhamughal02148@gmail.com>
2026-01-05 22:27:45 +0500

    add new files

---
+++ 02-Mongodb-Project-CRUD/index.js
@@ -0,0 +1,80 @@
+// Import express framework
+const express = require("express");
+
+// Import database connection function
+const dbConnection = require("./Create-Databse-Connection");
+
+// Create express application
+const app = express();
+
+// Middleware to read JSON data sent from frontend
+app.use(express.json());
+
+/*
+========================
+ INSERT DATA API
+========================
+*/
+
+// POST API to insert student data into database
+app.post("/insert-data", async (req, res) => {
+
+  // Connect to database
+  let db = await dbConnection();
+
+  // Get "Students" collection from database
+  let studentCollection = db.collection("Students");
+
+  // Read student name and age from request body
+  let { sName, sAge } = req.body;
+
+  // Create object to store in database
+  let obj = { sName, sAge };
+
+  // Insert data into Students collection
+  let insertData = await studentCollection.insertOne(obj);
+
+  // Response object sent to frontend
+  let frontEndResponse = {
+    status: 1,
+    msg: "Data insert Successfully",
+    insertData
+  };
+
+  // Send response to client
+  res.send(frontEndResponse);
+});
+
+/*
+========================
+ FETCH DATA API
+========================
+*/
+
+// GET API to fetch all students data
+app.get("/Fetch-data", async (req, res) => {
+
+  // Connect to database
+  let db = await dbConnection();
+
+  // Get "Students" collection from database
+  let studentCollection = db.collection("Students");
+
+  // Fetch all documents from Students collection
+  let FetchedData = await studentCollection.find().toArray();
+
+  // Response object sent to frontend
+  let frontEndResponse = {
+    status: 1,
+    msg: "Data Fetched Successfully",
+    FetchedData
+  };
+
+  // Send response to client
+  res.send(frontEndResponse);
+});
+
+// Start server on port 8000
+app.listen(8000, () => {
+  console.log("Server running on port 8000");
+});
